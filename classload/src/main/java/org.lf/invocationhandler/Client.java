8a2c4e7c0230a78d66cdec7eb078f7093ebaa6f3
liufudev <liufu_dev@qq.com>
2018-08-25 21:03:15 +0800

    add

--- classload/src/main/java/org.lf/invocationhandler/Client.java
+++ classload/src/main/java/org.lf/invocationhandler/Client.java
@@ -9,9 +9,18 @@ public class Client {
 
     public static void main(String[] args) {
         Subject subject = new RealSubject();
-        InvocationHandlerImpl handler = new InvocationHandlerImpl(subject);
         Class<? extends Subject> aClass = subject.getClass();
-        Subject proxyInstance = (Subject) Proxy.newProxyInstance(aClass.getClassLoader(), aClass.getInterfaces(), handler);
-        proxyInstance.request("liufuy");
+        Subject proxyInstance = (Subject) Proxy.newProxyInstance(aClass.getClassLoader(), aClass.getInterfaces(), (proxy, method, param) ->
+                                                                 {
+                                                                     System.out.println("my name is liufu");
+                                                                     method.invoke(subject, param);
+                                                                     return null;
+                                                                 }
+
+
+        );
+        proxyInstance.request("liufu");
+        System.out.println("subject的Class类是：" + proxyInstance.getClass().toString());
+        System.out.print("subject中的属性有：");
     }
 }
